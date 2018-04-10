b2a96097b93a1382489a8bdea33a5a96bc6e38ea
Arniu Tseng <arniu2006@gmail.com>
2018-04-11 00:13:16 +0800

    Fix #11

--- src/scale.js
+++ src/scale.js
@@ -6,8 +6,11 @@ export default function createScale (
   xs: [number, number],
   ys: [number, number]
 ): number => number {
-  const [x0, x1] = xs
   const [y0, y1] = ys
+
+  const sameX = xs[0] === xs[1]
+  const x0 = sameX ? xs[0] - 1 : xs[0]
+  const x1 = sameX ? xs[1] + 1 : xs[1]
 
   const slope = (y1 - y0) / (x1 - x0)
   const intercept = y0 - slope * x0
